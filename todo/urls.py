9ce88caad68d2ec37e5c386f9b07ac8091f130f5
sochun98 <sochun98@naver.com>
2025-02-07 20:27:25 +0900

    20250207

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from todo.apis import TodoCreateAPI, TodoDeleteAPI, TodoListAPI, TodoRetrieveAPI, TodoUpdateAPI
 from todo.views import todo_list, todo_detail, todo_detail_name
 
 
+# http://127.0.0.1:8000/todo/
 urlpatterns = [
+    path("create/", TodoCreateAPI.as_view()),
+    path("list/", TodoListAPI.as_view()),
+    path("retrieve/<int:pk>/", TodoRetrieveAPI.as_view()),
+    path("update/<int:pk>/", TodoUpdateAPI.as_view()),
+    path("delete/<int:pk>/", TodoDeleteAPI.as_view()),
     path("list/", todo_list),
     path("list/<int:pk>/", todo_detail),
     path("list/<str:name>/", todo_detail_name),
